c8005d1ba8598c0dad524d75bdae53496bbbb08a
anupriya0520 <147934661+anupriya0520@users.noreply.github.com>
2025-07-02 22:40:50 +0530

    Add files via upload

---
+++ conventions.java
@@ -0,0 +1,12 @@
+class conventions{
+      public static void main(String[] args){
+      String _Name ="anupriya";
+      String collegeName="CMR";
+      int $rollNumber=53;
+      System.out.println(_Name);
+      System.out.println(collegeName);
+      System.out.println($rollNumber);
+}
+}
+       
+      
